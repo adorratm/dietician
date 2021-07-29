d128ea3aba905a749c8441c45512f7aa4f56046d
ozgurkaracam <ozgurkaracam55@gmail.com>
2021-07-29 23:36:52 +0300

    dieticians search & pagination

--- pages/dieticians/index.vue
+++ pages/dieticians/index.vue
@@ -1,89 +1,141 @@
 <template>
-  <v-card
-    class="mx-auto"
-  >
+  <div>
+    <v-row>
+      <v-col
+        cols="12"
+        md="12"
+      >
+        <v-text-field
+          label="Diyetisyen Ara"
+          filled
+          v-model="searchText"
+          @change="search"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <div class="text-center"
+         v-if="loading">
+      <v-progress-circular
+        :size="70"
+        :width="7"
+        color="purple"
+        indeterminate
+        class="mx-auto"
+      ></v-progress-circular>
+    </div>
+    <v-card
+      v-else
+      class="mx-auto pb-4"
 
-    <v-list three-line v-if="dieticians">
-      <template v-for="(item,key) in dieticians">
-        <v-subheader
-          :key="key"
-          v-text="item.department"
-        ></v-subheader>
+    >
+
+      <div v-if="dieticians.length>0 && loading==false">
+        <v-list three-line>
+          <template v-for="(item,key) in dieticians">
+            <v-subheader
+              :key="item.name"
+              v-text="item.department"
+            ></v-subheader>
 
 
-        <v-list-item
-          :key="key"
-        >
-          <v-list-item-avatar>
-            <v-img src="https://www.crushpixel.com/big-static7/preview4/vector-sign-diet-dietitian-hand-154597.jpg"></v-img>
-          </v-list-item-avatar>
+            <v-list-item
+              :key="key"
+            >
+              <v-list-item-avatar>
+                <v-img
+                  src="https://www.crushpixel.com/big-static7/preview4/vector-sign-diet-dietitian-hand-154597.jpg"></v-img>
+              </v-list-item-avatar>
 
-          <v-list-item-content>
-            <v-list-item-title v-html="item.name"></v-list-item-title>
-            <v-list-item-subtitle class="d-flex justify-space-between" >
-              <div>
-                Telefon: {{item.phone}}
-                Email: {{item.email}}<br>
-                Poliklinik: {{item.hospitalName}}
-              </div>
-              <div>
-                <v-btn
-                  depressed
-                  color="primary"
-                >
-                  Randevu Al
-                </v-btn>
-              </div>
+              <v-list-item-content>
+                <v-list-item-title v-html="item.name"></v-list-item-title>
+                <v-list-item-subtitle class="d-flex justify-space-between">
+                  <div>
+                    Telefon: {{ item.phone }}
+                    Email: {{ item.email }}<br>
+                    Poliklinik: {{ item.hospitalName }}
+                  </div>
+                  <div>
+                    <v-btn
+                      depressed
+                      color="primary"
+                    >
+                      Randevu Al
+                    </v-btn>
+                  </div>
 
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-list>
-  </v-card>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
+
+
+        <div class="text-center">
+          <v-pagination
+            v-model="page"
+            :length="pageLength"
+            circle
+          ></v-pagination>
+        </div>
+      </div>
+      <div v-else class="text-center"> Uygun Sonuç Bulunamadı</div>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-  async mounted(){
-     await this.$axios.get(process.env.apiBaseUrl+"home/dieticians").then(res=>{
-      this.dieticians=res.data.data
+  async mounted() {
+    await this.$axios.get(process.env.apiBaseUrl + "home/dieticians").then(res => {
+      this.setDieticianss(res.data.data.data,res.data.data.links.length-2)
     })
-    console.log(this.dieticians)
+    this.loading = false
+  },
+  methods: {
+    async search() {
+      this.loading=true
+      await this.$axios.post(process.env.apiBaseUrl + "home/searchdieticians"
+        , {
+          'searchText': this.searchText
+        }).then(res => {
+        this.setDieticianss(res.data.data.data,res.data.data.links.length-2)
+      })
+      this.loading=false
+    },
+    setDieticianss(data,totalpage){
+      this.dieticians=data
+      this.pageLength=totalpage
+    }
+
+  },
+  watch: {
+    // async searchText() {
+    //   this.loading = true
+    //   await this.$axios.post(process.env.apiBaseUrl + "home/searchdieticians"
+    //     , {
+    //       'searchText': this.searchText
+    //     }).then(res => {
+    //     this.dieticians = res.data.data
+    //   })
+    //   this.loading = false
+    //
+    // },
+    async page(){
+      this.loading=true
+       await this.$axios.get(process.env.apiBaseUrl+'home/dieticians?page='+this.page).then(res=>{
+         this.setDieticianss(res.data.data.data,res.data.data.links.length-2)
+      })
+      this.loading=false
+    }
+
   },
   data: () => ({
-    dieticians:[],
+    searchText: '',
+    pageLength:1,
+    page:1,
+    loading: true,
+    dieticians: [],
     items: [
-      { header: 'Diyetisyenler ' },
-      {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-        title: 'Brunch this weekend?',
-        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
-      },
-      { divider: true, inset: true },
-      {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
-      },
-      { divider: true, inset: true },
-      {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-        title: 'Oui oui',
-        subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
-      },
-      { divider: true, inset: true },
-      {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-        title: 'Birthday gift',
-        subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
-      },
-      { divider: true, inset: true },
-      {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-        title: 'Recipe to try',
-        subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
-      },
     ],
   }),
 }
