559d2a01b281422f301ce4c4618fccd27f30ae71
Emre KILIÇ <emrekilic19983@gmail.com>
2021-09-24 09:52:27 +0300

    remove all old files

--- plugins/dotenv.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require( 'dotenv' );
-const dotenvParseVariables = require( 'dotenv-parse-variables' );
-
-let env = dotenv.config( {} )
-if ( env.error ) throw env.error;
-env = dotenvParseVariables( env.parsed );
-
-console.log( env );
